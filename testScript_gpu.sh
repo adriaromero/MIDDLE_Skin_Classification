fa36fd3727506ad600fbbe07a0d48286b5d0f4c9
iamrosmarin <adriaromero@me.com>
2016-10-11 17:45:35 -0400

    Adding files

---
+++ testScript_gpu.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -t 20                               # time limit of job, in minutes.. just using now for testing
+#SBATCH -p gpu                              # which queue to submit to
+#SBATCH -o gpu_python_%j.out                    # File to which STDOUT will be written
+#SBATCH -e gpu_python_%j.err                    # File to which STDERR will be written
+#SBATCH --mail-type=END                     # Type of email notification- BEGIN,END,FAIL,ALL
+#SBATCH --mail-user=jburdick2015@fau.edu    # Email to which notifications will be sent
+
+python mnhist_test_fromFile.py
---
+++ testScript_shortq.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -t 10                               # time limit of job, in minutes.. just using now for testing
+#SBATCH -p shortq                           # which queue to submit to
+#SBATCH -o python_%j.out                    # File to which STDOUT will be written
+#SBATCH -e python_%j.err                    # File to which STDERR will be written
+#SBATCH --mail-type=END                     # Type of email notification- BEGIN,END,FAIL,ALL
+#SBATCH --mail-user=jburdick2015@fau.edu    # Email to which notifications will be sent
+
+python testpython.py
