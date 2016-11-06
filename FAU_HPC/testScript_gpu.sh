05d58d7988e46b63a64a8a3ffd766779bed84f02
iamrosmarin <adriaromero@me.com>
2016-11-05 20:07:58 -0400

    Cleaning

--- FAU_HPC/testScript_gpu.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH -t 20                               # time limit of job, in minutes.. just using now for testing
-#SBATCH -p gpu                              # which queue to submit to
-#SBATCH -o gpu_python_%j.out                    # File to which STDOUT will be written
-#SBATCH -e gpu_python_%j.err                    # File to which STDERR will be written
-#SBATCH --mail-type=END                     # Type of email notification- BEGIN,END,FAIL,ALL
-#SBATCH --mail-user=fau.alopez@fau.edu    # Email to which notifications will be sent
-
-python mnist.py
