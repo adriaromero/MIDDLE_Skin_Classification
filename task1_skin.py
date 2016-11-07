ce7e458e90e230260655be62e444999926be7827
Adria <adriaromero@me.com>
2016-11-07 16:58:50 +0100

    add task1_skin.py

--- task1_skin.py
+++ task1_skin.py
@@ -55,7 +55,7 @@ print('Data augmentation...')
 print('-'*30)
 # this is the augmentation configuration we will use for testing:
 # only rescaling
-test_datagen = ImageDataGenerator(rescale=1./255)
+test_datagen = ImageDataGenerator(rescale=1./255, shear_range=0.2,zoom_range=0.2,horizontal_flip=True)
 
 print('-'*30)
 print('Creating train batches...')
